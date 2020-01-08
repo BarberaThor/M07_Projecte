97f8ab7b3cdf3d8b7e59f3a06b63fa4d33bff7b9
lubo1 <lubo18600@gmail.com>
2020-01-08 10:54:45 +0100

    Implementació CardView en el gradle i modificació del java DetailActicity.

--- app/src/main/java/cat/barbera/m07_projecte/DetailActivity.java
+++ app/src/main/java/cat/barbera/m07_projecte/DetailActivity.java
@@ -3,6 +3,9 @@ package cat.barbera.m07_projecte;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.bumptech.glide.Glide;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        TextView assignaturaTitle = findViewById(R.id.titleDetail);
+        ImageView assignaturaImage = findViewById(R.id.imageDetail);
+
+        assignaturaTitle.setText(getIntent().getStringExtra("title"));
+
+        Glide.with(this).load(getIntent().getIntExtra("image_resource",0))
+                .into(assignaturaImage);
     }
 }
