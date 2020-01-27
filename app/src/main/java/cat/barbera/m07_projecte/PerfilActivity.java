4211117827a8dc9137edaf0fb23d2a720174dca3
lubo1 <lubo18600@gmail.com>
2020-01-27 10:53:07 +0100

    Creacion de la Activity para mostrar el Perfil del Usuario. Implementacion del layout con sus botones,etc... y del archivo java con su handler.

---
+++ app/src/main/java/cat/barbera/m07_projecte/PerfilActivity.java
@@ -0,0 +1,19 @@
+package cat.barbera.m07_projecte;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class PerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+    }
+
+    private void handle_Guardar(View view) {
+
+    }
+}
