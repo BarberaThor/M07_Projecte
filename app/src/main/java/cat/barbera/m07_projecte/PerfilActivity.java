dfe770c0ed5aac739a3d79d845f4f197c4580faf
lubo1 <lubo18600@gmail.com>
2020-01-27 12:16:02 +0100

    Implementacio dels strings per a la traducció del llenguatge.

--- app/src/main/java/cat/barbera/m07_projecte/PerfilActivity.java
+++ app/src/main/java/cat/barbera/m07_projecte/PerfilActivity.java
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class PerfilActivity extends AppCompatActivity {
+
+    private RadioButton mAngles, mCatala, mCastella;
+    private RadioGroup mGroupRdb;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,7 @@ public class PerfilActivity extends AppCompatActivity {
     }
 
     private void handle_Guardar(View view) {
+        Toast.makeText(this, "Les dades s'han actualitzat correctament", Toast.LENGTH_SHORT).show();
 
     }
 }
